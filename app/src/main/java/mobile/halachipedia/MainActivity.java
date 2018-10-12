d452b94bc653bab15c4129d89f316536ebb280b5
MDjavaheri <michael.djavaheri@mail.yu.edu>
2018-10-12 09:03:12 +0300

    initial commit - set up basic app

---
+++ app/src/main/java/mobile/halachipedia/MainActivity.java
@@ -0,0 +1,37 @@
+package mobile.halachipedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.halachipedia.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
